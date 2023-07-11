f1c26a2eea8c6038d87f20b502467b71f1b42490
Earl A. Villapaz <jasonburning16@gmail.com>
2023-07-11 18:57:28 +0800

    adding the first billboard route

---
+++ app/api/[storeId]/billboards/route.ts
@@ -0,0 +1,83 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+    req: Request,
+    {params}: { params: {storeId: string} }
+) { 
+    try {
+        const body = await req.json();
+        const { userId } = auth();
+        const { label, imageUrl } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthenticated", { status: 401 });
+        }
+
+        if (!label) {
+            return new NextResponse("Missing label", { status: 400 });
+        }
+
+        if (!imageUrl) {
+            return new NextResponse("Missing imageUrl", { status: 400 });
+        }
+
+        if (!params.storeId) {
+            return new NextResponse("Missing Store ID", { status: 400 });
+        }
+
+        const storeByUserId = await prismadb.store.findFirst({
+            where: {
+                id: params.storeId,
+                userId
+            }
+        });
+        
+        if (!storeByUserId) {
+            return new NextResponse("Unauthorized User", { status: 403 });
+        }
+
+        const billboard = await prismadb.billboard.create({
+            data: {
+                label,
+                imageUrl,
+                storeId: params.storeId,
+            }
+        });
+
+        return NextResponse.json(billboard);
+
+    } catch (error) {
+        console.log("[BILLBOARD_POST]", error);
+        return (
+            new NextResponse("Internal Server Error", { status: 500})
+        );
+    }
+};
+
+export async function GET(
+    req: Request,
+    {params}: { params: {storeId: string} }
+) { 
+    try {
+        if (!params.storeId) {
+            return new NextResponse("Missing Store ID", { status: 400 });
+        }
+
+        const billboards = await prismadb.billboard.findMany({
+            where: {
+                storeId: params.storeId,
+            }
+        });
+
+        return NextResponse.json(billboards);
+
+    } catch (error) {
+        console.log("[BILLBOARD_GET]", error);
+        return (
+            new NextResponse("Internal Server Error", { status: 500})
+        );
+    }
+};
